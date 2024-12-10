1ebc360d40c693a4570bd77c27f655730906b381
0ambar <abrahamr677@gmail.com>
2024-12-09 23:37:40 -0600

    axios y context añadidos

--- Cartilla/src/Login.jsx
+++ Cartilla/src/Login.jsx
@@ -1,16 +1,60 @@
 // Login.jsx
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import clienteAxios from './config/axios'
 import { useNavigate } from 'react-router-dom';
 import './Login.css';
 
+// Context
+import { CRMContext } from './context/CRMContext';
+
 const Login = () => {
+  
+  // Auth y token
+  const [auth, guardarAuth] = useContext(CRMContext);
+
+  // State con los datos del formulario
+  const [credenciales, guardarCredenciales] = useState({});
+
+
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  // inicar sesion en el servidor
+  const handleSubmit = async (e) => {
+
     e.preventDefault();
-    // Aquí va la lógica para validar el login
-    navigate('/usuario');
+    
+    // autenticar al usuario
+    try {
+        const respuesta = await clienteAxios.post('/usuario/login', credenciales);
+        
+        // extraer el token y colocarlo en localstorage
+        const { token } = respuesta.data;
+        localStorage.setItem('token', token);
+  
+        // colocarlo en el state
+        guardarAuth({
+            token, 
+            auth: true
+        })
+  
+        // alerta
+        console.log('Login Correcto');
+        
+      } catch (error) {
+        console.log(error);
+        return navigate('/login');
+      }
+      
+    navigate('/usuario/perfil');
   };
+
+  // almacenar lo que el usuario escribe en el state
+  const leerDatos = e => {
+    guardarCredenciales({
+        ...credenciales,
+        [e.target.name] : e.target.value
+    })
+  }
 
   return (
     <div className="login-container">
@@ -24,9 +68,17 @@ const Login = () => {
         <form onSubmit={handleSubmit}>
           <h2>Bienvenido a la Cartilla de Salud Digital</h2>
           <label>Usuario</label>
-          <input type="text" placeholder="correoejemplo@gmail.com" />
+          <input type="text" 
+            name='email'
+            placeholder="correoejemplo@gmail.com" 
+            onChange={leerDatos}
+          />
           <label>Contraseña</label>
-          <input type="password" placeholder="********" />
+          <input type="password" 
+            name='password' 
+            placeholder="********" 
+            onChange={leerDatos}
+          />
           <div className="options">
             <label>
               <input type="checkbox" /> Recuerdame
