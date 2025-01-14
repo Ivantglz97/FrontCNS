8bd0f44a4d53b413acf7eedab4850ac5aa46c064
0ambar <abrahamr677@gmail.com>
2025-01-13 22:11:32 -0600

    componente de escaneo actualizado

--- src/EscanearDatosG.jsx
+++ src/EscanearDatosG.jsx
@@ -3,6 +3,8 @@ import './EscanearDatosG.css'
 
 import { BrowserMultiFormatReader } from '@zxing/library'; // Importar la librería de QR
 
+import clienteAxios from './config/axios';
+
 // Datos estáticos para usuarios con ID 1 y 2
 const usuarios = [
   {
@@ -42,12 +44,26 @@ const DatosG = () => {
   const videoRef = useRef(null); // Referencia al video de la cámara
   const codeReader = useRef(null); // Referencia para el lector de códigos QR
 
-  const handleScan = (data) => {
+  const handleScan = async (data) => {
     if (data) {
       const id = data.getText(); // Extraer el texto (ID) del código QR
       setQrData(id); // Guardar el ID escaneado
-      const usuario = usuarios.find((user) => user.id.toString() === id); // Buscar el usuario con ese ID
+
+      let usuario = null;
+
+      try {
+        const respuesta = await clienteAxios.get(`/personal/ver-paciente/${id}`);
+
+        usuario = respuesta.data;
+
+
+      } catch (error) {
+        console.error('Error al cargar datos:', error);
+      }
+
+      // const usuario = usuarios.find((user) => user.id.toString() === id); // Buscar el usuario con ese ID
       if (usuario) {
+        console.log('Usuario encontrado desde EscanearDatosG: ', usuario);
         setUserData(usuario); // Cargar los datos del usuario correspondiente
       } else {
         setUserData(null); // Si no se encuentra el ID, limpiar los datos
@@ -85,7 +101,6 @@ const DatosG = () => {
     }
     setIsCameraActive(false); // Marcar la cámara como inactiva
     setQrData(null); // Limpiar los datos escaneados
-    setUserData(null); // Limpiar los datos del usuario
   };
 
   // Iniciar el lector al cargar el componente
@@ -101,85 +116,75 @@ const DatosG = () => {
   }, [isCameraActive]);
 
   return (
-        <div className="card">
-              <div className="card-body">
-                <div className="left-column">
-                <h3>Identificación</h3>
-                  <div className="form-group">
-                    <label htmlFor="curp">CURP</label>
-                    <input type="text" id="curp" name="curp" value={userData ? userData.curp : ''} readOnly />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="nombre">Nombre y Apellidos</label>
-                    <input type="text" id="nombre" name="nombre" value={userData ? userData.nombre : ''} readOnly />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="afiliacion">No. de Afiliación</label>
-                    <input type="text" id="afiliacion" name="afiliacion" value={userData ? userData.afiliacion : ''} readOnly />
-                  </div>
+    <div className="card">
+      <div className="card-body">
+        <div className="left-column">
 
-                  <h3>Domicilio</h3>
-                  <div className="form-group">
-                    <label htmlFor="calleNumero">Calle y Número</label>
-                    <input type="text" id="calleNumero" name="calleNumero" value={userData ? userData.domicilio : ''} readOnly />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="colonia">Colonia/Localidad</label>
-                    <input type="text" id="colonia" name="colonia" value={userData ? userData.colonia : ''} readOnly />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="municipio">Municipio o Alcaldía</label>
-                    <input type="text" id="municipio" name="municipio" value={userData ? userData.municipio : ''} readOnly />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="codigoPostal">Código Postal</label>
-                    <input type="text" id="codigoPostal" name="codigoPostal" value={userData ? userData.codigoPostal : ''} readOnly />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="entidadFederativa">Entidad Federativa</label>
-                    <input type="text" id="entidadFederativa" name="entidadFederativa" value={userData ? userData.entidadFederativa : ''} readOnly />
-                  </div>
+          <h3>Identificación</h3>
+          <div className="form-group">
+            <label htmlFor="curp">CURP</label>
+            <input type="text" id="curp" name="curp" value={userData ? userData.curp : ''} readOnly />
+          </div>
+          <div className="form-group">
+            <label htmlFor="nombre">Nombre</label>
+            <input type="text" id="nombre" name="nombre" value={userData ? `${userData.nombre} ${userData.apellidoPaterno} ${userData.apellidoMaterno}` : ''} readOnly />
+          </div>
+          <div className="form-group">
+            <label htmlFor="afiliacion">No. de Afiliación</label>
+            <input type="text" id="afiliacion" name="afiliacion" value={userData ? userData.afiliacion : ''} readOnly />
+          </div>
 
+          <h3>Domicilio</h3>
+          <div className="form-group">
+            <label htmlFor="calleNumero">Calle y Número</label>
+            <input type="text" id="calleNumero" name="calleNumero" value={userData ? userData.domicilio : ''} readOnly />
+          </div>
+          <div className="form-group">
+            <label htmlFor="colonia">Colonia/Localidad</label>
+            <input type="text" id="colonia" name="colonia" value={userData ? userData.Asentamiento.d_asenta : ''} readOnly />
+          </div>
+          <div className="form-group">
+            <label htmlFor="municipio">Municipio o Alcaldía</label>
+            <input type="text" id="municipio" name="municipio" value={userData ? userData.Asentamiento.D_mnpio : ''} readOnly />
+          </div>
+          <div className="form-group">
+            <label htmlFor="codigoPostal">Código Postal</label>
+            <input type="text" id="codigoPostal" name="codigoPostal" value={userData ? userData.Asentamiento.d_codigo : ''} readOnly />
+          </div>
+          <div className="form-group">
+            <label htmlFor="entidadFederativa">Entidad Federativa</label>
+            <input type="text" id="entidadFederativa" name="entidadFederativa" value={userData ? userData.Asentamiento.d_estado : ''} readOnly />
+          </div>
+        </div>
+
+        <div className="right-column">
+          <h3>Lugar y Fecha de Nacimiento</h3>
+          <div className="form-group">
+            <label htmlFor="lugarNacimiento">Lugar de Nacimiento</label>
+            <input type="text" id="lugarNacimiento" name="lugarNacimiento" value={userData ? userData.lugarNacimiento : ''} readOnly />
+          </div>
+          <div className="form-group">
+            <label htmlFor="fechaNacimiento">Fecha de Nacimiento</label>
+            <input type="date" id="fechaNacimiento" name="fechaNacimiento" value={userData ? userData.fechaNacimiento : ''} readOnly />
+          </div>
+
+
+          <div className="qr-card">
+            <h2>Escanear Código QR</h2>
+            {!isCameraActive ? (
+              <button className="start-camera-button" onClick={startScanner}>Iniciar Cámara</button>
+            ) : (
+              <div>
+                <div className="qr-container">
+                  <video ref={videoRef} style={{ width: '100%' }} />
                 </div>
-
-                <div className="right-column">
-                  <h3>Lugar y Fecha de Nacimiento</h3>
-                  <div className="form-group">
-                    <label htmlFor="lugarNacimiento">Municipio o Alcaldía</label>
-                    <input type="text" id="lugarNacimiento" name="lugarNacimiento" value={userData ? userData.lugarNacimiento : ''} readOnly />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="entidadFederativaNacimiento">Entidad Federativa</label>
-                    <input type="text" id="entidadFederativaNacimiento" name="entidadFederativaNacimiento" value={userData ? userData.entidadFederativa : ''} readOnly />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="fechaNacimiento">Fecha de Nacimiento</label>
-                    <input type="date" id="fechaNacimiento" name="fechaNacimiento" value={userData ? userData.fechaNacimiento : ''} readOnly />
-                  </div>
-
-
-                  <div className="qr-card">
-                  <h2>Escanear Código QR</h2>
-                  {!isCameraActive ? (
-                    <button className="start-camera-button" onClick={startScanner}>Iniciar Cámara</button>
-                  ) : (
-                    <div>
-                      <div className="qr-container">
-                        <video ref={videoRef} style={{ width: '100%' }} />
-                      </div>
-                      <button className="stop-camera-button" onClick={stopScanner}>Detener Cámara</button>
-                    </div>
-                  )}
-                  
-                </div>
-
-                </div>
-
+                <button className="stop-camera-button" onClick={stopScanner}>Detener Cámara</button>
               </div>
-            
-
-
-    </div>
+            )}
+          </div>
+        </div >
+      </div >
+    </div >
   );
 };
 
